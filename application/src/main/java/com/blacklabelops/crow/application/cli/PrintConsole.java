58aef2b00469f0e31e2d1749a656bb8e98cb21af
Steffen Bleul <sbl@blacklabelops.com>
2018-04-27 10:05:43 +0200

    Message string pruned for job listing.

--- application/src/main/java/com/blacklabelops/crow/application/cli/PrintConsole.java
+++ application/src/main/java/com/blacklabelops/crow/application/cli/PrintConsole.java
@@ -6,27 +6,41 @@ import java.util.List;
 
 import com.blacklabelops.crow.application.rest.JobInformation;
 import com.blacklabelops.crow.application.util.VersionAccessor;
+import com.cronutils.utils.StringUtils;
 
 public class PrintConsole {
 
 	public void printJobs(JobInformation[] jobs) {
-		String columnFormat = "%1$-15s%2$-15s%3$-25s%4$-12s%5$-12s";
-		String header = String.format(columnFormat, "NAME","CRON","NEXT","EXECUTION","ON ERROR");
+		String columnFormat = "%1$-30s%2$-15s%3$-25s%4$-12s%5$-12s";
+		String header = String.format(columnFormat, "NAME", "CRON", "NEXT", "EXECUTION", "ON ERROR");
 		System.out.println(header);
 		for (JobInformation job : jobs) {
 			String nextExecution = new SimpleDateFormat("yyyy/MM/dd HH:mm z").format(job.getNextExecution());
-			String row = String.format(columnFormat, job.getName(),job.getCron(),nextExecution,job.getExecution(),job.getErrorMode());
+			String row = String.format(columnFormat,
+					prune(job.getName(), 30),
+					prune(job.getCron(), 15),
+					prune(nextExecution, 25),
+					prune(job.getExecution(), 12),
+					prune(job.getErrorMode(), 12));
 			System.out.println(row);
 		}
 	}
-	
+
+	private Object prune(String name, int i) {
+		String pruned = "";
+		if (!StringUtils.isEmpty(name)) {
+			pruned = name.substring(0, Math.min(name.length(), i));
+		}
+		return pruned;
+	}
+
 	public void printHelp() {
 		System.out.println();
 		for (String row : createHelp()) {
 			System.out.println(row);
 		}
 	}
-	
+
 	private String[] createHelp() {
 		List<String> help = new ArrayList<>();
 		String commandFormat = "%-20s%-30s";
@@ -37,7 +51,7 @@ public class PrintConsole {
 		help.add(String.format(commandFormat, "help", "Print help."));
 		return help.toArray(new String[help.size()]);
 	}
-	
+
 	public void printVersion(String serverVersion) {
 		System.out.println("Client Version: " + new VersionAccessor().getVersion());
 		System.out.println("Server Version: " + serverVersion);
